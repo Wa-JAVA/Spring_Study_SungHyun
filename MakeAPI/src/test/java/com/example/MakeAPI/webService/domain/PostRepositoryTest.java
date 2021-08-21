18f6441f828e3f0000f2a6cb71d406833182a1d1
Ssunghyn <75001450+Ssunghyn@users.noreply.github.com>
2021-08-22 00:00:25 +0900

    SpringBoot Study  Second Day#2
    JPA로 간단한 API 만들기

---
+++ MakeAPI/src/test/java/com/example/MakeAPI/webService/domain/PostRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.example.MakeAPI.webService.domain;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @After()
+    public void cleanup() {
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시물저장_불러오기() {
+        //given
+        postRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("han990102@naver.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle(), is("테스트 게시글"));
+        assertThat(posts.getContent(), is("테스트 본문"));
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        postRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("han990102@naver.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertTrue(posts.getCreatedDate().isAfter(now));
+        assertTrue(posts.getModifiedDate().isAfter(now));
+    }
+}
